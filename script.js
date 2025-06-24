b84e6cd345ab8130180070f655d5a5f83c4c62be
ariellyr <arielly.r@aluno.senia.br>
2025-06-24 15:37:54 -0300

    iniciante

--- script.js
+++ script.js
@@ -5,15 +5,16 @@ const resultado = document.getElementById ("resultado");
 
 tabuadaform.addEventListener('submit', function(event){
     event.preventDefault()
-    calcularTabuada(numero.vaule) 
+    calcularTabuada(numero.value)
 })
 function calcularTabuada(numero){
-    for(i=1;i<=10;i++){ 
-   let conta = i * numero
-        let linha = document.createElement("p");
-        linha.textContent = `${numero} x ${i} = ${conta}`
-       resultado.appendChild(linha)
-}
+    for(i=1;i<=10;i++){
+    let conta = i * numero
+    let linha = document.createElement("p")
+    linha.textContent = `${numero} x ${i} = ${conta}`
+resultado.appendChild(linha)
+    }
+    
 
 }
- 
+
